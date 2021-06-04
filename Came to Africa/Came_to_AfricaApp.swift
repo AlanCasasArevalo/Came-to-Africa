c8b6098187c3827446550cb339456f3c7a5ecf42
Alan Casas <alancasasarevalo@gmail.com>
2021-06-04 19:59:42 +0200

    Initial Commit

---
+++ Came to Africa/Came_to_AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Came_to_AfricaApp.swift
+//  Came to Africa
+//
+//  Created by Alan Casas on 04/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct Came_to_AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
